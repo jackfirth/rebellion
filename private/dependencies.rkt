fd8942ce8fadcedeed09e8ac3d9bbc2b3ebf448f
Jack Firth <jackhfirth@gmail.com>
2018-10-30 23:03:14 -0700

    Add low-dependency name implementation
    For use in predicates

--- private/dependencies.rkt
+++ private/dependencies.rkt
@@ -1,3 +1,48 @@
 #lang racket/base
 
 (module reader syntax/module-reader rebellion/private/dependencies)
+
+(require racket/contract/base
+         racket/format)
+
+(provide ->
+         #%app
+         #%datum
+         #%module-begin
+         #%top
+         #%top-interaction
+         ~a
+         ~s
+         ~v
+         any/c
+         boolean?
+         cond
+         contract-out
+         contract?
+         define
+         define-logger
+         define-values
+         else
+         equal?
+         gen:custom-write
+         if
+         listof
+         make-struct-type-property
+         module+
+         not
+         or
+         or/c
+         output-port?
+         provide
+         quote
+         require
+         struct
+         struct-type-property/c
+         submod
+         symbol->string
+         symbol?
+         unless
+         void
+         void?
+         write-string
+         λ)
