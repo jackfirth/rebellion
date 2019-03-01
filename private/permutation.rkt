301055209159e10ef3d1cc7017caa029bf1ba7a0
Jack Firth <jackhfirth@gmail.com>
2019-03-01 00:16:07 -0800

    Add permutations

---
+++ private/permutation.rkt
@@ -0,0 +1,135 @@
+#lang racket/base
+
+(require racket/contract/base)
+
+(provide
+ permutation
+ cyclic-permutation
+ (contract-out
+  [permutation? (-> any/c boolean?)]
+  [permutation-ref
+   (->i ([perm () permutation?]
+         [pos (perm) (and/c natural? (</c (permutation-size perm)))])
+        [_ (perm) (and/c natural? (</c (permutation-size perm)))])]
+  [permutation-size (-> permutation? natural?)]
+  [string-permute
+   (->i ([str () (and/c string? immutable?)]
+         [perm (str) (permutation-size/c (string-length str))])
+        [_ () (and/c string? immutable?)])]
+  [vector-permute
+   (->i ([vec () (and/c vector? immutable?)]
+         [perm (vec) (permutation-size/c (vector-length vec))])
+        [_ () (and/c vector? immutable?)])]))
+
+(require (for-syntax racket/base
+                     racket/list)
+         racket/contract/combinator
+         racket/math
+         racket/struct
+         rebellion/private/struct-definition-util
+         rebellion/struct-descriptor
+         rebellion/struct-equal-property
+         syntax/parse/define)
+
+(module+ test
+  (require (submod "..")
+           rackunit))
+
+;@------------------------------------------------------------------------------
+
+(define (permutation-size/c expected-size)
+  (flat-named-contract
+   (list 'permutation-size/c expected-size)
+   (λ (perm)
+     (and (permutation? perm)
+          (equal? (permutation-size perm) expected-size)))))
+
+(define (make-permutation-properties descriptor)
+  (list (cons prop:equal+hash
+              (make-struct-equal+hash-property descriptor))
+        (cons prop:custom-write
+              (make-constructor-style-printer
+               (λ (_) 'permutation)
+               (λ (this) (permutation-vector this))))))
+
+(define permutation-descriptor
+  (make-struct-type/descriptor
+   #:name 'permutation
+   #:immutable-fields 1
+   #:property-maker make-permutation-properties))
+
+(define permutation-constructor
+  (struct-descriptor-constructor permutation-descriptor))
+
+(define permutation? (struct-descriptor-predicate permutation-descriptor))
+
+(define-struct-field-accessors permutation (vector)
+  #:descriptor permutation-descriptor)
+
+(define (permutation-size perm) (vector-length (permutation-vector perm)))
+(define (permutation-ref perm pos) (vector-ref (permutation-vector perm) pos))
+
+(define-for-syntax (check-duplicate-position positions)
+  (check-duplicates (syntax->list positions) #:key syntax-e))
+
+(define-for-syntax (check-position-bounds positions size)
+  (findf (λ (pos) (>= (syntax-e pos) size)) (syntax->list positions)))
+
+(define-simple-macro (permutation position:nat ...)
+  #:do [(define size (length (syntax->list #'(position ...))))]
+  #:fail-when (check-position-bounds #'(position ...) size)
+  (format "position out of bounds for permutation of size ~a" size)
+  #:fail-when (check-duplicate-position #'(position ...)) "duplicate position"
+  (permutation-constructor (vector-immutable position ...)))
+
+(define-simple-macro (cyclic-permutation position:nat ...+)
+  #:do [(define positions
+          (map syntax-e (syntax->list #'(position ...))))
+        (define size (add1 (apply max 0 positions)))
+        (define positions-vec
+          (vector->immutable-vector (list->vector positions)))
+        (define num-positions (vector-length positions-vec))]
+  #:fail-when (check-duplicate-position #'(position ...)) "duplicate position"
+  #:do [(define vec (build-vector size values))
+        (for ([i (in-range 0 num-positions)])
+          (define j (modulo (add1 i) num-positions))
+          (define pos (vector-ref positions-vec i))
+          (define next-pos (vector-ref positions-vec j))
+          (vector-set! vec next-pos pos))]
+  #:with vec #`(quote #,(vector->immutable-vector vec))
+  (permutation-constructor vec))
+
+(define (vector-permute vec perm)
+  (vector->immutable-vector
+   (build-vector (vector-length vec)
+                 (λ (pos)
+                   (vector-ref vec (permutation-ref perm pos))))))
+
+(define (string-permute str perm)
+  (string->immutable-string
+   (build-string (string-length str)
+                 (λ (pos)
+                   (string-ref str (permutation-ref perm pos))))))
+
+(module+ test
+  (define perm (permutation 3 5 4 0 1 2))
+  (test-case "permutation-size"
+    (check-equal? (permutation-size perm) 6))
+  (test-case "permutation-ref"
+    (check-equal? (permutation-ref perm 0) 3)
+    (check-equal? (permutation-ref perm 1) 5)
+    (check-equal? (permutation-ref perm 2) 4)
+    (check-equal? (permutation-ref perm 3) 0)
+    (check-equal? (permutation-ref perm 4) 1)
+    (check-equal? (permutation-ref perm 5) 2))
+  (test-case "vector-permute"
+    (define vec (vector-immutable 'a 'b 'c 'd 'e 'f))
+    (check-equal? (vector-permute vec perm)
+                  (vector-immutable 'd 'f 'e 'a 'b 'c)))
+  (test-case "string-permute"
+    (check-pred immutable? (string-permute "abcdef" perm))
+    (check-equal? (string-permute "abcdef" perm) "dfeabc"))
+  (test-case "cyclic-permutation"
+    (define perm (cyclic-permutation 0 2 4 6 8))
+    (check-equal? (permutation-size perm) 9)
+    (check-equal? (string-permute "abcdefhij" perm) "jbadcfeih")))
---
+++ private/permutation.scrbl
@@ -0,0 +1,90 @@
+#lang scribble/manual
+
+@(require (for-label racket/base
+                     racket/contract/base
+                     rebellion/permutation)
+          rebellion/private/scribble-evaluator-factory
+          scribble/example)
+
+@(define make-evaluator
+   (make-module-sharing-evaluator-factory
+    #:public (list 'rebellion/permutation)
+    #:private (list 'racket/base)))
+
+@title{Permutations}
+@defmodule[rebellion/permutation]
+
+A @deftech{permutation} is a rearrangement of a finite set, with elements of the
+set represented by natural numbers. More formally, a permutation of size
+@racket[_n] is a bijection from the set of natural numbers less than @racket[_n]
+to itself. Permutations are useful for efficiently expressing how collections
+are arranged, ordered, and sorted. Two permutations are @racket[equal?] if they
+have the same size and rearrange elements in the same way.
+
+@defproc[(permutation? [v any/c]) boolean?]{
+ A predicate for @tech{permutations}.}
+
+@defform[(permutation position ...)]{
+ Constructs a @tech{permutation} that moves the @racket[_n]th item in a
+ collection to the @racket[_n]th @racket[position], with indices starting from
+ zero. The resulting permutation's size is equal to the number of @racket[
+ position]s given, and no duplicates are allowed. This form is equivalent to the
+ standard @deftech{one-line notation} for permutations in mathematics.
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (string-permute "abcd" (permutation 0 2 1 3))
+   (string-permute "abcd" (permutation 3 2 1 0)))}
+
+@defform[(cyclic-permutation position ...)]{
+ Constructs a @tech{permutation} that moves the item at each @racket[position]
+ to the next @racket[position] immediately following it. The item at the last
+ @racket[position] is moved to the first @racket[position]. The resulting
+ permutation's size is equal to one plus the largest @racket[position] given,
+ and no duplicates are allowed. This form is equivalent to the standard
+ @deftech{cyclic notation} for permutations in mathematics.
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (string-permute "abcdefghijklmnopqrstuvwxyz"
+                   (cyclic-permutation 5 17 25 8 0)))}
+
+@defproc[(permutation-size [perm permutation?]) natural?]{
+ Returns the size of @racket[perm].
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (permutation-size (permutation 0 1 2 3 4 5))
+   (permutation-size (cyclic-permutation 6 18 4 3 11)))}
+
+@defproc[(permutation-ref [perm permutation?]
+                          [pos (and/c natural? (</c (permutation-size perm)))])
+         (and/c natural? (</c (permutation-size perm)))]{
+ Returns the resulting position of item @racket[pos] when permuted by @racket[
+ perm].
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (define p (permutation 3 8 2 7 1 6 0 5 4))
+   (permutation-ref p 0)
+   (permutation-ref p 3)
+   (permutation-ref p 8))}
+
+@defproc[(vector-permute [vec (and/c vector? immutable?)] [perm permutation?])
+         (and/c vector? immutable?)]{
+ Rearranges the elements of @racket[vec] according to @racket[perm], which must
+ have the same size as @racket[vec].
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (vector-permute (vector-immutable 'a 'b 'c 'd 'e 'f)
+                   (permutation 4 1 5 3 0 2)))}
+
+@defproc[(string-permute [str (and/c string? immutable?)] [perm permutation?])
+         (and/c string? immutable?)]{
+ Rearranges the characters of @racket[str] according to @racket[perm], which
+ must have the same size as @racket[str].
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (string-permute "abcdefg" (cyclic-permutation 4 5 6)))}
