f79a918bfff7af155b906026b05d4fa8f212c8c2
Jack Firth <jackhfirth@gmail.com>
2019-03-02 04:28:48 -0800

    Add tuple types

---
+++ private/tuple-type.rkt
@@ -0,0 +1,293 @@
+#lang racket/base
+
+(require racket/contract/base)
+
+(provide
+ (contract-out
+  [initialized-tuple-descriptor? (-> any/c boolean?)]
+  [make-default-tuple-properties
+   (-> uninitialized-tuple-descriptor?
+       (listof (cons/c struct-type-property? any/c)))]
+  [make-tuple-field-accessor
+   (->i ([desc () tuple-descriptor?]
+         [pos (desc)
+              (and/c natural?
+                     (</c (tuple-type-size (tuple-descriptor-type desc))))])
+        ([field-name symbol?])
+        [_ (desc) (-> (tuple-descriptor-predicate desc) any/c)])]
+  [make-tuple-type
+   (->* (tuple-type?)
+        (#:guard (or/c procedure? #f)
+         #:inspector inspector?
+         #:property-maker
+         (-> uninitialized-tuple-descriptor?
+             (listof (cons/c struct-type-property? any/c))))
+        initialized-tuple-descriptor?)]
+  [tuple-descriptor? (-> any/c boolean?)]
+  [tuple-descriptor-accessor (-> tuple-descriptor? (-> any/c natural? any/c))]
+  [tuple-descriptor-constructor (-> tuple-descriptor? procedure?)]
+  [tuple-descriptor-predicate (-> tuple-descriptor? (-> any/c boolean?))]
+  [tuple-descriptor-struct-type (-> initialized-tuple-descriptor? struct-type?)]
+  [tuple-descriptor-type (-> tuple-descriptor? tuple-type?)]
+  [tuple-type
+   (->* (symbol? natural?)
+        (#:predicate-name symbol?
+         #:constructor-name symbol?
+         #:accessor-name symbol?)
+        tuple-type?)]
+  [tuple-type? (-> any/c boolean?)]
+  [tuple-type-accessor-name (-> tuple-type? symbol?)]
+  [tuple-type-constructor-name (-> tuple-type? symbol?)]
+  [tuple-type-name (-> tuple-type? symbol?)]
+  [tuple-type-predicate-name (-> tuple-type? symbol?)]
+  [tuple-type-size (-> tuple-type? natural?)]
+  [uninitialized-tuple-descriptor? (-> any/c boolean?)]))
+
+(require racket/math
+         racket/struct
+         rebellion/keyset
+         rebellion/permutation
+         rebellion/private/struct-definition-util
+         rebellion/struct-descriptor
+         rebellion/struct-equal-property
+         rebellion/struct-write-property)
+
+(module+ test
+  (require (submod "..")
+           racket/format
+           rackunit))
+
+;@------------------------------------------------------------------------------
+
+(define (make-transparent-style-properties descriptor)
+  (define equal+hash (make-struct-equal+hash-property descriptor))
+  (define custom-write
+    (make-constructor-style-struct-write-property descriptor))
+  (list (cons prop:equal+hash equal+hash)
+        (cons prop:custom-write custom-write)))
+
+(define (make-descriptor-style-properties descriptor)
+  (define accessor (struct-descriptor-accessor descriptor))
+  (define type-name (struct-descriptor-name descriptor))
+  (define prefix (string-append "#<" (symbol->string type-name) ":"))
+  (define equal+hash (make-struct-equal+hash-property descriptor))
+  (define (get-name this) (symbol->string (tuple-type-name (accessor this 0))))
+  (define (custom-write this out _)
+    (write-string prefix out)
+    (write-string (get-name this) out)
+    (write-string ">" out))
+  (list (cons prop:equal+hash equal+hash)
+        (cons prop:custom-write custom-write)))
+
+(define (make-default-tuple-properties descriptor)
+  (define type (tuple-descriptor-type descriptor))
+  (define accessor (tuple-descriptor-accessor descriptor))
+  (define name (tuple-type-name type))
+  (define size (tuple-type-size type))
+  (define equal+hash (make-equal+hash-property size accessor))
+  (define custom-write
+    (make-constructor-style-printer
+     (λ (_) name)
+     (λ (this) (build-list size (λ (pos) (accessor this pos))))))
+  (list (cons prop:equal+hash equal+hash)
+        (cons prop:custom-write custom-write)))
+
+;@------------------------------------------------------------------------------
+
+(define fields:tuple-type
+  (keyset #:name
+          #:size
+          #:predicate-name
+          #:constructor-name
+          #:accessor-name))
+
+(define fields:uninitialized-tuple-descriptor
+  (keyset #:type #:predicate #:constructor #:accessor))
+
+(define fields:initialized-tuple-descriptor
+  (keyset #:type #:predicate #:constructor #:accessor #:struct-type))
+
+(define descriptor:tuple-type
+  (make-struct-type/descriptor
+   #:name 'tuple-type
+   #:immutable-fields (keyset-size fields:tuple-type)
+   #:constructor-name 'constructor:tuple-type
+   #:property-maker make-transparent-style-properties))
+
+(define descriptor:uninitialized-tuple-descriptor
+  (make-struct-type/descriptor
+   #:name 'uninitialized-tuple-descriptor
+   #:immutable-fields (keyset-size fields:uninitialized-tuple-descriptor)
+   #:constructor-name 'constructor:uninitialized-tuple-descriptor
+   #:property-maker make-descriptor-style-properties))
+
+(define descriptor:initialized-tuple-descriptor
+  (make-struct-type/descriptor
+   #:name 'initialized-tuple-descriptor
+   #:immutable-fields (keyset-size fields:initialized-tuple-descriptor)
+   #:constructor-name 'constructor:initialized-tuple-descriptor
+   #:property-maker make-descriptor-style-properties))
+
+(define tuple-type? (struct-descriptor-predicate descriptor:tuple-type))
+
+(define constructor:tuple-type
+  (struct-descriptor-constructor descriptor:tuple-type))
+
+(define (tuple-type
+         name
+         size
+         #:predicate-name [predicate-name (default-tuple-predicate-name name)]
+         #:constructor-name [constructor-name name]
+         #:accessor-name [accessor-name (default-tuple-accessor-name name)])
+  (constructor:tuple-type
+   name size predicate-name constructor-name accessor-name))
+
+(define-struct-field-accessors tuple-type
+  (name size predicate-name constructor-name accessor-name)
+  #:descriptor descriptor:tuple-type)
+
+(define uninitialized-tuple-descriptor?
+  (struct-descriptor-predicate descriptor:uninitialized-tuple-descriptor))
+
+(define constructor:uninitialized-tuple-descriptor
+  (struct-descriptor-constructor descriptor:uninitialized-tuple-descriptor))
+
+(define (uninitialized-tuple-descriptor #:type type
+                                        #:predicate predicate
+                                        #:constructor constructor
+                                        #:accessor accessor)
+  (constructor:uninitialized-tuple-descriptor
+   type predicate constructor accessor))
+
+(define-struct-field-accessors uninitialized-tuple-descriptor
+  (type predicate constructor accessor)
+  #:descriptor descriptor:uninitialized-tuple-descriptor)
+
+(define initialized-tuple-descriptor?
+  (struct-descriptor-predicate descriptor:initialized-tuple-descriptor))
+
+(define constructor:initialized-tuple-descriptor
+  (struct-descriptor-constructor descriptor:initialized-tuple-descriptor))
+
+(define (initialized-tuple-descriptor #:type type
+                                      #:struct-type struct-type
+                                      #:predicate predicate
+                                      #:constructor constructor
+                                      #:accessor accessor)
+  (constructor:initialized-tuple-descriptor
+   type predicate constructor accessor struct-type))
+
+(define-struct-field-accessors initialized-tuple-descriptor
+  (type predicate constructor accessor struct-type)
+  #:descriptor descriptor:initialized-tuple-descriptor)
+
+;@------------------------------------------------------------------------------
+
+(define (tuple-descriptor? v)
+  (or (initialized-tuple-descriptor? v)
+      (uninitialized-tuple-descriptor? v)))
+
+(define (tuple-descriptor-case descriptor #:initialized f #:uninitialized g)
+  (if (initialized-tuple-descriptor? descriptor)
+      (f descriptor)
+      (g descriptor)))
+
+(define (tuple-descriptor-type descriptor)
+  (tuple-descriptor-case descriptor
+                         #:initialized initialized-tuple-descriptor-type
+                         #:uninitialized uninitialized-tuple-descriptor-type))
+
+(define (tuple-descriptor-struct-type descriptor)
+  (uninitialized-tuple-descriptor-type descriptor))
+
+(define (tuple-descriptor-predicate descriptor)
+  (tuple-descriptor-case
+   descriptor
+   #:initialized initialized-tuple-descriptor-predicate
+   #:uninitialized uninitialized-tuple-descriptor-predicate))
+
+(define (tuple-descriptor-constructor descriptor)
+  (tuple-descriptor-case
+   descriptor
+   #:initialized initialized-tuple-descriptor-constructor
+   #:uninitialized uninitialized-tuple-descriptor-constructor))
+
+(define (tuple-descriptor-accessor descriptor)
+  (tuple-descriptor-case
+   descriptor
+   #:initialized initialized-tuple-descriptor-accessor
+   #:uninitialized uninitialized-tuple-descriptor-accessor))
+
+;@------------------------------------------------------------------------------
+
+(define (make-tuple-type
+         type
+         #:guard [guard #f]
+         #:inspector [inspector (current-inspector)]
+         #:property-maker [prop-maker make-default-tuple-properties])
+  (define (get-predicate descriptor)
+    (procedure-rename (struct-descriptor-predicate descriptor)
+                      (tuple-type-predicate-name type)))
+  (define (get-constructor descriptor)
+    (procedure-rename (struct-descriptor-constructor descriptor)
+                      (tuple-type-constructor-name type)))
+  (define (get-accessor descriptor)
+    (procedure-rename (struct-descriptor-accessor descriptor)
+                      (tuple-type-accessor-name type)))
+  (define (struct-prop-maker descriptor)
+    (prop-maker
+     (uninitialized-tuple-descriptor
+      #:type type
+      #:predicate (get-predicate descriptor)
+      #:constructor (get-constructor descriptor)
+      #:accessor (get-accessor descriptor))))
+  (define descriptor
+    (make-struct-type/descriptor
+     #:name (tuple-type-name type)
+     #:immutable-fields (tuple-type-size type)
+     #:constructor-name (tuple-type-constructor-name type)
+     #:guard guard
+     #:inspector inspector
+     #:property-maker struct-prop-maker))
+  (initialized-tuple-descriptor
+   #:type type
+   #:struct-type (struct-descriptor-type descriptor)
+   #:predicate (get-predicate descriptor)
+   #:constructor (get-constructor descriptor)
+   #:accessor (get-accessor descriptor)))
+
+(define (default-tuple-predicate-name name)
+  (string->symbol (format "~a?" name)))
+
+(define (default-tuple-accessor-name name)
+  (string->symbol (format "~a-ref" name)))
+
+(define (make-tuple-field-accessor descriptor pos [field-name* #f])
+  (define type (tuple-descriptor-type descriptor))
+  (define accessor (tuple-descriptor-accessor descriptor))
+  (define name (tuple-type-name type))
+  (define field-name (or field-name* (string->symbol (format "field~a" pos))))
+  (define field-accessor-name (string->symbol (format "~a-~a" name field-name)))
+  (procedure-rename (λ (this) (accessor this pos)) field-accessor-name))
+
+(module+ test
+  (test-case "make-tuple-type"
+    (define point-type (tuple-type 'point 2))
+    (define point-descriptor (make-tuple-type point-type))
+    (define point (tuple-descriptor-constructor point-descriptor))
+    (define point? (tuple-descriptor-predicate point-descriptor))
+    (define point-x (make-tuple-field-accessor point-descriptor 0 'x))
+    (define point-y (make-tuple-field-accessor point-descriptor 1 'y))
+    (define p (point 42 1000))
+    (check-pred point? p)
+    (check-equal? p (point 42 1000))
+    (check-equal? (point-x p) 42)
+    (check-equal? (point-y p) 1000)
+    (check-equal? (~v p) "(point 42 1000)")
+    (check-equal? (~a p) "#<point: 42 1000>")
+    (check-equal? (~s p) "#<point: 42 1000>")
+    (check-equal? (~v point-descriptor) "#<initialized-tuple-descriptor:point>")
+    (check-equal? (~v point) "#<procedure:point>")
+    (check-equal? (~v point?) "#<procedure:point?>")
+    (check-equal? (~v point-x) "#<procedure:point-x>")
+    (check-equal? (~v point-y) "#<procedure:point-y>")))
---
+++ private/tuple-type.scrbl
@@ -0,0 +1,128 @@
+#lang scribble/manual
+
+@(require (for-label racket/base
+                     racket/math
+                     rebellion/tuple-type
+                     rebellion/struct-descriptor)
+          rebellion/private/scribble-evaluator-factory
+          scribble/examples)
+
+@(define make-evaluator
+   (make-module-sharing-evaluator-factory
+    #:public (list 'rebellion/tuple-type)
+    #:private (list 'racket/base)))
+
+@title{Tuple Types}
+@defmodule[rebellion/tuple-type]
+
+A @deftech{tuple type} is a data type representing immutable fixed-size
+sequences of unnamed values, each of which is a @deftech{tuple instance} of the
+tuple type. All instances of a tuple type are the same size, and that size is
+accessible from the type using @racket[tuple-type-size].
+
+Tuple types are specifications, not implementations. A tuple type does not
+directly provide a constructor or accessor; it only provides information about
+the type such as its symbolic name, the number of fields, etc. An actual
+implementation of a tuple type is obtained via @racket[make-tuple-type], which
+generates a new implementation of a tuple type and returns a @deftech{tuple type
+ descriptor} object. Descriptors provide functions that implement the type and
+a predicate that identifies the implementation's values. Descriptors are
+generative; calling @racket[make-tuple-type] with the same tuple type will make
+multiple distinct implementations of that type.
+
+@defproc[(tuple-type? [v any/c]) boolean?]{
+ A predicate for @tech{tuple types}.}
+
+@defproc[
+ (tuple-type [name symbol?]
+             [size natural?]
+             [#:predicate-name predicate-name (or/c symbol? #f) #f]
+             [#:constructor-name constructor-name (or/c symbol? #f) #f]
+             [#:accessor-name accessor-name (or/c symbol? #f) #f])
+ tuple-type?]{
+ Constructs a @tech{tuple type} of size @racket[size] and named @racket[name].
+ The optional @racket[predicate-name], @racket[constructor-name], and @racket[
+ accessor-name] arguments control the result of @racket[object-name] on the
+ functions implementing the type. If not provided, @racket[predicate-name]
+ defaults to @racket[name]@verbatim|{?}|, @racket[constructor-name] defaults to
+ @racket[name], and @racket[accessor-name] defaults to @racket[
+ name]@verbatim|{-ref}|. Two tuple types constructed with the same arguments are
+ @racket[equal?]. To make an implementation of a tuple type, see @racket[
+ make-tuple-type].}
+
+@deftogether[[
+ @defproc[(tuple-type-name [type tuple-type?]) symbol?]
+ @defproc[(tuple-type-size [type tuple-type?]) natural?]
+ @defproc[(tuple-type-predicate-name [type tuple-type?]) symbol?]
+ @defproc[(tuple-type-constructor-name [type tuple-type?]) symbol?]
+ @defproc[(tuple-type-accessor-name [type tuple-type?]) symbol?]]]{
+ Accessors for the various fields of a @tech{tuple type}.}
+
+@defproc[(tuple-descriptor? [v any/c]) boolean?]{
+ A predicate for @tech{tuple type descriptors}.}
+
+@deftogether[[
+ @defproc[(initialized-tuple-descriptor? [v any/c]) boolean?]
+ @defproc[(uninitialized-tuple-descriptor? [v any/c]) boolean?]]]{
+ Predicates for @tech{tuple type descriptors} that either have or haven't been
+ initialized yet. The predicate, constructor, and accessor of an uninitialized
+ tuple descriptor @bold{must not be called until initialization is complete}, as
+ the implementations of those functions won't exist until then. Initialization
+ of a descriptor completes when @racket[make-tuple-type] returns.}
+
+@deftogether[[
+ @defproc[(tuple-descriptor-type [descriptor tuple-descriptor?]) tuple-type?]
+ @defproc[(tuple-descriptor-predicate [descriptor tuple-descriptor?])
+          (-> any/c boolean?)]
+ @defproc[(tuple-descriptor-constructor [descriptor tuple-descriptor?])
+          procedure?]
+ @defproc[(tuple-descriptor-accessor [descriptor tuple-descriptor?])
+          (-> (tuple-descriptor-predicate descriptor) natural? any/c)]]]{
+ Accessors for the various fields of a @tech{tuple type descriptor}.}
+
+@defproc[
+ (make-tuple-type [type tuple-type?]
+                  [#:guard guard (or/c procedure? #f) #f]
+                  [#:inspector inspector inspector? (current-inspector)]
+                  [#:property-maker prop-maker
+                   (-> uninitialized-tuple-descriptor?
+                       (listof (cons/c struct-type-property? any/c)))
+                   make-default-tuple-properties])
+ initialized-tuple-descriptor?]{
+ Implements @racket[type] and returns a @tech{tuple type descriptor} for the
+ new implementation. The @racket[guard] and @racket[inspector] arguments behave
+ the same as the corresponding arguments of @racket[make-struct-type], although
+ there are no transparent or prefab tuple types. The @racket[prop-maker]
+ argument is similar to the corresponding argument of @racket[
+ make-struct-type/descriptor]. By default, tuple types are created with
+ properties that make them print and compare in a manner similar to transparent
+ structure types --- see @racket[make-default-tuple-properties] for details.
+
+ @(examples
+   #:eval (make-evaluator) #:once
+   (define point-descriptor (make-tuple-type (tuple-type 'point 2)))
+   (define point (tuple-descriptor-constructor point-descriptor))
+   (define point-x (make-tuple-field-accessor point-descriptor 0 'x))
+   (define point-y (make-tuple-field-accessor point-descriptor 1 'y))
+   (point 42 888)
+   (point-x (point 42 888))
+   (point-y (point 42 888)))}
+
+@defproc[
+ (make-tuple-field-accessor [descriptor tuple-descriptor?]
+                            [pos natural?]
+                            [name (or/c symbol? #f)
+                             (symbol->string (format "field~a" pos))])
+ (-> (tuple-descriptor-predicate descriptor) any/c)]{
+ Builds a field accessor function that returns the @racket[pos] field of
+ instances of the @tech{tuple type} implemented by @racket[descriptor]. If
+ @racket[name] is provided, it is used to derive the name of the function for
+ debugging purposes. See @racket[make-tuple-type] for usage examples.}
+
+@defproc[
+ (make-default-tuple-properties [descriptor uninitialized-tuple-descriptor?])
+ (listof (cons/c struct-type-property? any/c))]{
+ Returns implementations of @racket[prop:equal+hash] and @racket[
+ prop:custom-write] suitable for most @tech{tuple types}. This function is
+ called by @racket[make-tuple-type] when no @racket[_prop-maker] argument is
+ supplied.}
