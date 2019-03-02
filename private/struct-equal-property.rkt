782cf416cdd7c5bef12b7f53091ce3e053715918
Jack Firth <jackhfirth@gmail.com>
2019-03-02 04:28:31 -0800

    Add undocumented make-equal+hash-property utility

--- private/struct-equal-property.rkt
+++ private/struct-equal-property.rkt
@@ -4,31 +4,38 @@
 
 (provide
  (contract-out
+  [make-equal+hash-property
+   (-> natural? (-> any/c natural? any/c)
+       (list/c procedure? procedure? procedure?))]
   [make-struct-equal+hash-property
    (-> uninitialized-struct-descriptor?
        (list/c procedure? procedure? procedure?))]))
 
-(require rebellion/generative-token
+(require racket/math
+         rebellion/generative-token
          rebellion/struct-descriptor)
 
 ;@------------------------------------------------------------------------------
 
+(define (make-equal+hash-property size accessor)
+  (define token (make-generative-token))
+  (define (equal-proc this other recur)
+    (for/and ([pos (in-range size)])
+      (recur (accessor this pos) (accessor other pos))))
+  (define (hash-proc this recur)
+    (let loop ([pos size] [previous-vs (list token)])
+      (cond
+        [(zero? pos) (recur previous-vs)]
+        [else
+         (define next-pos (sub1 pos))
+         (loop next-pos (cons (accessor this next-pos) previous-vs))])))
+  (define hash2-proc hash-proc)
+  (list equal-proc hash-proc hash2-proc))
+  
 (define (make-struct-equal+hash-property descriptor)
   (define accessor (struct-descriptor-accessor descriptor))
-  (define fields
+  (define size
     (+ (struct-descriptor-mutable-fields descriptor)
        (struct-descriptor-immutable-fields descriptor)
        (struct-descriptor-auto-fields descriptor)))
-  (define token (make-generative-token))
-  (define (equal-proc this other recur)
-    (for/and ([field (in-range fields)])
-      (recur (accessor this field) (accessor other field))))
-  (define (hash-proc this recur)
-    (let loop ([field fields] [previous-vs (list token)])
-      (cond
-        [(zero? field) (recur previous-vs)]
-        [else
-         (define next-field (sub1 field))
-         (loop next-field (cons (accessor this next-field) previous-vs))])))
-  (define hash2-proc hash-proc)
-  (list equal-proc hash-proc hash2-proc))
+  (make-equal+hash-property size accessor))
