cc87ac03d32637695a05758d6563e56da89e497e
Jack Firth <jackhfirth@gmail.com>
2019-03-02 18:37:17 -0800

    Add web links

---
+++ private/web-link.rkt
@@ -0,0 +1,27 @@
+#lang racket/base
+
+(require racket/contract/base)
+
+(provide
+ (contract-out
+  [web-link (-> url? (or/c url? symbol?) url? web-link?)]
+  [web-link? (-> any/c boolean?)]
+  [web-link-source (-> web-link? url?)]
+  [web-link-relation (-> web-link? (or/c url? symbol?))]
+  [web-link-target (-> web-link? url?)]))
+
+(require net/url
+         rebellion/tuple-type)
+
+(define web-link-descriptor (make-tuple-type (tuple-type 'web-link 3)))
+(define web-link (tuple-descriptor-constructor web-link-descriptor))
+(define web-link? (tuple-descriptor-predicate web-link-descriptor))
+
+(define web-link-source
+  (make-tuple-field-accessor web-link-descriptor 0 'source))
+
+(define web-link-relation
+  (make-tuple-field-accessor web-link-descriptor 1 'relation))
+
+(define web-link-target
+  (make-tuple-field-accessor web-link-descriptor 2 'target))
