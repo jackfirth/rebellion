b1b8d90f18e215ce948370947fcd06d432100194
Jack Firth <jackhfirth@gmail.com>
2019-03-06 21:22:32 -0800

    Wrap rebellion/private/scribble-evaluator-factory in a doc submodule

--- private/scribble-evaluator-factory.rkt
+++ private/scribble-evaluator-factory.rkt
@@ -1,39 +1,41 @@
 #lang racket/base
 
-(require racket/contract/base)
+(module doc racket/base
 
-(provide
- (contract-out
-  [evaluator/c contract?]
-  [evaluator-factory? (-> any/c boolean?)]
-  [evaluator-factory-create (-> evaluator-factory? evaluator/c)]
-  [make-module-sharing-evaluator-factory
-   (->* ()
-        (#:private (listof module-path?)
-         #:public (listof module-path?))
-        evaluator-factory?)]))
+  (require racket/contract/base)
 
-(require racket/list
-         scribble/example)
+  (provide
+   (contract-out
+    [evaluator/c contract?]
+    [evaluator-factory? (-> any/c boolean?)]
+    [evaluator-factory-create (-> evaluator-factory? evaluator/c)]
+    [make-module-sharing-evaluator-factory
+     (->* ()
+          (#:private (listof module-path?)
+           #:public (listof module-path?))
+          evaluator-factory?)]))
 
-;@------------------------------------------------------------------------------
+  (require racket/list
+           scribble/example)
 
-(define evaluator/c (-> any/c any/c))
+  ;@----------------------------------------------------------------------------
 
-(struct evaluator-factory (thunk)
-  #:constructor-name make-evaluator-factory
-  #:property prop:procedure 0)
+  (define evaluator/c (-> any/c any/c))
 
-(define (evaluator-factory-create factory)
-  ((evaluator-factory-thunk factory)))
+  (struct evaluator-factory (thunk)
+    #:constructor-name make-evaluator-factory
+    #:property prop:procedure 0)
 
-(define (make-module-sharing-evaluator-factory
-         #:public [public-modules empty]
-         #:private [private-modules empty])
-  (define base-factory
-    (make-base-eval-factory (append private-modules public-modules)))
-  (define (factory)
-    (define evaluator (base-factory))
-    (evaluator `(require ,@public-modules))
-    evaluator)
-  (make-evaluator-factory factory))
+  (define (evaluator-factory-create factory)
+    ((evaluator-factory-thunk factory)))
+
+  (define (make-module-sharing-evaluator-factory
+           #:public [public-modules empty]
+           #:private [private-modules empty])
+    (define base-factory
+      (make-base-eval-factory (append private-modules public-modules)))
+    (define (factory)
+      (define evaluator (base-factory))
+      (evaluator `(require ,@public-modules))
+      evaluator)
+    (make-evaluator-factory factory)))
