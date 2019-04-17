3efe61f1c07eed5b8ccf5f6074e06822fe76d98a
Jack Firth <jackhfirth@gmail.com>
2019-04-16 23:36:53 -0700

    Add basic rebellion/private/media implementation
    Docs forthcoming

---
+++ private/media.rkt
@@ -0,0 +1,187 @@
+#lang racket/base
+
+;; TODO: add docs and rebellion/media reprovide module
+
+(require racket/contract/base)
+
+(provide
+ (contract-out
+  [media (-> media-type? immutable-bytes? media?)]
+  [media? predicate/c]
+  [media-bytes (-> media? immutable-bytes?)]
+  [media-get-type (-> media? media-type?)]
+  [media-type
+   ;; TODO: improve checking and handling of allowed characters / length
+   ;;   restrictions / case sensitivity / etc. and put an actual contract on the
+   ;;   field values of the parameters record
+   (->* (interned-symbol? interned-symbol?)
+        (#:tree (or/c interned-symbol? #f)
+         #:suffix (or/c interned-symbol? #f)
+         #:parameters record?)
+        media-type?)]
+  [media-type-top-level (-> media-type? interned-symbol?)]
+  [media-type-subtype (-> media-type? interned-symbol?)]
+  [media-type-tree (-> media-type? (or/c interned-symbol? #f))]
+  [media-type-suffix (-> media-type? (or/c interned-symbol? #f))]
+  [media-type-parameters (-> media-type? record?)]
+  [media-type->string (-> media-type? immutable-string?)]
+  [string->media-type (-> immutable-string? media-type?)]))
+
+(require racket/string
+         racket/struct
+         rebellion/equal+hash/tuple
+         rebellion/record
+         rebellion/tuple-type
+         rebellion/tuple-type-definition)
+
+(module+ test
+  (require rackunit))
+
+;@------------------------------------------------------------------------------
+;; utilities
+
+;; TODO: move this into a string utils module
+(define (empty-string? v) (and (string? v) (not (non-empty-string? v))))
+
+;; TODO: move this into a symbol utils module
+(define (interned-symbol? v) (and (symbol? v) (symbol-interned? v)))
+
+;; TODO: add modules for immutable-by-default bytes and strings and add separate
+;;   modules for mutable bytes and strings, like how lists and mlists are split
+(define (immutable-bytes? v) (and (bytes? v) (immutable? v)))
+(define (immutable-string? v) (and (string? v) (immutable? v)))
+
+;@------------------------------------------------------------------------------
+;; media-type
+;; TODO: use better representations of top-level types, trees, and structured
+;;   suffixes, instead of just allowing arbitrary symbols
+
+(define (make-media-type-properties descriptor)
+  (define name (tuple-type-name (tuple-descriptor-type descriptor)))
+  (define accessor (tuple-descriptor-accessor descriptor))
+  (define equal+hash (make-tuple-equal+hash descriptor))
+  (define custom-write
+    (make-constructor-style-printer
+     (λ (_) name)
+     (λ (this) (media-type->string this))))
+  (list (cons prop:equal+hash equal+hash)
+        (cons prop:custom-write custom-write)))
+
+(define-tuple-type media-type (top-level tree subtype suffix parameters)
+  #:constructor constructor:media-type
+  #:property-maker make-media-type-properties)
+
+(define (media-type top-level subtype
+                    #:tree [tree #f]
+                    #:suffix [suffix #f]
+                    #:parameters [parameters empty-record])
+  ;; TODO: media type instances should probably be interned
+  (constructor:media-type top-level tree subtype suffix parameters))
+
+;; TODO: figure out how to correctly deal with the fact that RFC 6838 doesn't
+;;   actually say how parameter values should be parsed or serialized, and it
+;;   doesn't even assume that parameter values are strings (as opposed to
+;;   arbitrary binary data)
+(define (media-type->string type)
+  (define top-level (media-type-top-level type))
+  (define subtype (media-type-subtype type))
+  (define tree (media-type-tree type))
+  (define suffix (media-type-suffix type))
+  (define parameters (media-type-parameters type))
+  (string-append (symbol->string top-level)
+                 "/"
+                 (if tree (string-append (symbol->string tree) ".") "")
+                 (symbol->string subtype)
+                 (if suffix (string-append "+" (symbol->string suffix)) "")
+                 (media-type-parameters->string parameters)))
+
+(define (media-type-parameters->string parameters)
+  (define param-strings
+    (for/list ([k (in-list (record-keywords parameters))])
+      (define param-raw (record-ref parameters k))
+      (define param-name (keyword->string k))
+      (define param-quoted
+        (media-type-parameter-value->possibly-quoted-string param-raw))
+      (string-append param-name "=" param-quoted)))
+  (define sep "; ")
+  (define joined (string-join param-strings sep))
+  (if (non-empty-string? joined)
+      (string-append sep joined)
+      joined))
+
+(define (media-type-parameter-value->possibly-quoted-string param-value-string)
+  ;; TODO: add backslash quoting (see RFC 7230 Section 3.2.6)
+  (if (or (for/or ([c (in-string param-value-string)])
+            (media-type-parameter-value-char-requires-quoting? c))
+          (empty-string? param-value-string))
+      (string-append "\"" param-value-string "\"")
+      param-value-string))
+
+(define (media-type-parameter-value-char-requires-quoting? char)
+  ;; TODO: use a better data structure here
+  (not (string-contains? rfc7230-section-3.2.6-tchar-legal-alphabet
+                         (string char))))
+
+(define rfc7230-section-3.2.6-tchar-legal-alphabet
+  (string-append "abcdefghijklmnopqrstuvwxyz"
+                 "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                 "0123456789"
+                 "!#$%&'*+-.^_`|~"))
+
+(define (string->media-type s)
+  ;; TODO: implement media-type parser based on HTTP Content-Type header grammar
+  (error 'unimplemented))
+
+(module+ test
+  (test-case "media-type-with-only-required-parts"
+    (define mt (media-type 'video 'example))
+    (check-equal? (media-type->string mt) "video/example"))
+  (test-case "media-type-with-tree"
+    (define mt (media-type 'application 'coffeescript #:tree 'vnd))
+    (check-equal? (media-type->string mt) "application/vnd.coffeescript"))
+  (test-case "media-type-with-one-unquoted-param"
+    (define mt
+      (media-type 'text 'plain #:parameters (record #:charset "utf-8")))
+    (check-equal? (media-type->string mt) "text/plain; charset=utf-8"))
+  (test-case "media-type-with-one-quoted-param"
+    (define mt
+      (media-type 'application 'example
+                  #:parameters (record #:param "this param has whitespace")))
+    (check-equal? (media-type->string mt)
+                  "application/example; param=\"this param has whitespace\""))
+  (test-case "media-type-with-multiple-params"
+    (define mt
+      (media-type 'text 'example
+                  #:parameters (record #:charset "utf-8"
+                                       #:other-param "other-value")))
+    (check-equal? (media-type->string mt)
+                  "text/example; charset=utf-8; other-param=other-value")))
+
+;@------------------------------------------------------------------------------
+;; media
+
+(define type:media (tuple-type 'media 2))
+(define index:media:type 0)
+(define index:media:bytes 1)
+(define descriptor:media (tuple-type-make-implementation type:media))
+
+(define media (tuple-descriptor-constructor descriptor:media))
+(define media? (tuple-descriptor-predicate descriptor:media))
+
+;; TODO(https://github.com/jackfirth/rebellion/issues/75): improve type
+;;   definition macros so this isn't necessary anymore
+(define media-get-type
+  (procedure-rename
+   (make-tuple-field-accessor descriptor:media index:media:type 'type)
+   'media-get-type))
+
+(define media-bytes
+  (make-tuple-field-accessor descriptor:media index:media:bytes 'bytes))
+
+(module+ test
+  (test-case "media"
+    (define text/plain
+      (media-type 'text 'plain #:parameters (record #:charset "utf-8")))
+    (define m (media text/plain #"hello world"))
+    (check-equal? (media-get-type m) text/plain)
+    (check-equal? (media-bytes m) #"hello world")))
