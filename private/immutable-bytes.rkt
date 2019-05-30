218e816b12d0cfe965b84f7ee9568b814f15c36e
Jack Firth <jackhfirth@gmail.com>
2019-05-30 01:35:11 -0700

    Add more immutable bytes functions

--- private/immutable-bytes.rkt
+++ private/immutable-bytes.rkt
@@ -4,8 +4,184 @@
 
 (provide
  (contract-out
-  [immutable-bytes? predicate/c]))
+  [immutable-bytes? predicate/c]
+  [make-immutable-bytes (-> natural? byte? immutable-bytes?)]
+  [immutable-bytes (-> byte? ... immutable-bytes?)]
+  [immutable-bytes-length (-> immutable-bytes? natural?)]
+  [immutable-bytes-ref (-> immutable-bytes? natural? byte?)]
+  [immutable-subbytes
+   (->* (immutable-bytes? natural?) (natural?) immutable-bytes?)]
+  [immutable-bytes-append (-> immutable-bytes? ... immutable-bytes?)]
+  [immutable-bytes->list (-> immutable-bytes? (listof byte?))]
+  [list->immutable-bytes (-> (listof byte?) immutable-bytes?)]
+  [immutable-bytes=? bytes-comparison-function/c]
+  [immutable-bytes<? bytes-comparison-function/c]
+  [immutable-bytes>? bytes-comparison-function/c]
+  [immutable-bytes->string/utf-8 bytes-encoder/c]
+  [immutable-bytes->string/locale bytes-encoder/c]
+  [immutable-bytes->string/latin-1 bytes-encoder/c]
+  [immutable-string->bytes/utf-8 bytes-decoder/c]
+  [immutable-string->bytes/locale bytes-decoder/c]
+  [immutable-string->bytes/latin-1 bytes-decoder/c]
+  [immutable-string-utf-8-length
+   (->* (immutable-string?) (natural? natural?) natural?)]
+  [immutable-bytes-utf-8-length
+   (->* (immutable-bytes?) (err-char/c natural? natural?)
+        (or/c natural? #f))]
+  [immutable-bytes-utf-8-ref
+   (->* (immutable-bytes? natural?) (err-char/c natural? natural?)
+        (or/c char? #f))]
+  [immutable-bytes-utf-8-index
+   (->* (immutable-bytes? natural?) (err-char/c natural? natural?)
+        (or/c natural? #f))]
+  [immutable-bytes-convert
+   (->* (bytes-converter? immutable-bytes?)
+        (natural? natural?)
+        (values immutable-bytes? natural? bytes-conversion-status/c))]
+  [immutable-bytes-convert-end (-> bytes-converter? immutable-bytes?)]
+  [immutable-bytes-append*
+   (-> immutable-bytes? ... (listof immutable-bytes?) immutable-bytes?)]
+  [immutable-bytes-join
+   (-> (listof immutable-bytes?) immutable-bytes? immutable-bytes?)]
+  [empty-immutable-bytes empty-immutable-bytes?]
+  [empty-immutable-bytes? predicate/c]
+  [nonempty-immutable-bytes? predicate/c]))
+
+(require (for-syntax racket/base
+                     racket/string)
+         racket/math
+         racket/bytes
+         rebellion/immutable-string
+         syntax/parse/define)
 
 ;@------------------------------------------------------------------------------
 
 (define (immutable-bytes? v) (and (bytes? v) (immutable? v)))
+
+(define bytes-comparison-function/c
+  (-> immutable-bytes? immutable-bytes? ... boolean?))
+
+(define err-char/c (or/c char? #f))
+
+(define bytes-encoder/c
+  (->* (immutable-bytes?) (err-char/c natural? natural?)
+       immutable-string?))
+
+(define bytes-decoder/c
+  (->* (immutable-string?) ((or/c byte? #f) natural? natural?)
+       immutable-bytes?))
+
+(define bytes-conversion-status/c
+  (or/c 'complete 'continues 'aborts 'error))
+
+(define empty-immutable-bytes #"")
+
+(define (empty-immutable-bytes? v)
+  (and (equal? v empty-immutable-bytes) (immutable? v)))
+
+(define (nonempty-immutable-bytes? v)
+  (and (immutable-bytes? v) (not (equal? v empty-immutable-bytes))))
+
+;@------------------------------------------------------------------------------
+
+(define immutable-bytes-length bytes-length)
+(define immutable-bytes-ref bytes-ref)
+(define immutable-bytes->list bytes->list)
+(define immutable-bytes=? bytes=?)
+(define immutable-bytes<? bytes<?)
+(define immutable-bytes>? bytes>?)
+(define immutable-string-utf-8-length string-utf-8-length)
+(define immutable-bytes-utf-8-length bytes-utf-8-length)
+(define immutable-bytes-utf-8-ref bytes-utf-8-ref)
+
+;@------------------------------------------------------------------------------
+
+(begin-for-syntax
+  (define-syntax-class positional-argument
+    (pattern id:id)
+    (pattern [id:id default:expr]))
+  
+  (define-syntax-class bytes-function-header
+    #:attributes (id lambda-argument-binders function-call-expression)
+    
+    (pattern (id:id arg:positional-argument ...)
+      #:with lambda-argument-binders #'(arg ...)
+      #:with function-call-expression #'(id arg.id ...))
+    
+    (pattern (id:id . rest-arg:id)
+      #:with lambda-argument-binders #'rest-arg
+      #:with function-call-expression #'(apply id rest-arg))
+    
+    (pattern (id:id arg:positional-argument ...+ . rest-arg:id)
+      #:with lambda-argument-binders #'(arg ... . rest-arg)
+      #:with function-call-expression #'(apply id arg.id ... rest-arg))))
+
+(define-simple-macro
+  (define-wrapper header:bytes-function-header)
+  #:do [(define id-str (symbol->string (syntax-e #'header.id)))
+        (define imm-str (string-replace id-str "bytes" "immutable-bytes"))]
+  #:with immutable-id (datum->syntax #'header.id (string->symbol imm-str))
+  (define immutable-id
+    (λ header.lambda-argument-binders
+      (bytes->immutable-bytes header.function-call-expression))))
+
+;@------------------------------------------------------------------------------
+
+(define-wrapper (make-bytes size b))
+(define-wrapper (bytes . bs))
+(define-wrapper (bytes-append . bstrs))
+(define-wrapper (list->bytes lst))
+(define-wrapper (bytes-append* . bstrs+list))
+(define-wrapper (bytes-join bstrs sep))
+
+;@------------------------------------------------------------------------------
+
+(define (immutable-subbytes bstr start [end (immutable-bytes-length bstr)])
+  (bytes->immutable-bytes (subbytes bstr start end)))
+
+(define ((make-encoder mutable-encoder)
+         bstr [err-char #f] [start 0] [end (immutable-bytes-length bstr)])
+  (string->immutable-string (mutable-encoder bstr err-char start end)))
+
+(define ((make-decoder mutable-decoder)
+         str [err-byte #f] [start 0] [end (immutable-string-length str)])
+  (bytes->immutable-bytes (mutable-decoder str err-byte start end)))
+
+(define immutable-bytes->string/utf-8 (make-encoder bytes->string/utf-8))
+(define immutable-bytes->string/locale (make-encoder bytes->string/locale))
+(define immutable-bytes->string/latin-1 (make-encoder bytes->string/latin-1))
+(define immutable-string->bytes/utf-8 (make-decoder string->bytes/utf-8))
+(define immutable-string->bytes/locale (make-decoder string->bytes/locale))
+(define immutable-string->bytes/latin-1 (make-decoder string->bytes/latin-1))
+
+(define (immutable-bytes-convert converter
+                                 bstr
+                                 [start 0]
+                                 [end (immutable-bytes-length bstr)])
+  (define-values (output-bstr amount-read? status)
+    (bytes-convert converter bstr start end))
+  (values (bytes->immutable-bytes output-bstr) amount-read? status))
+
+(define bytes-convert-end-error-message
+  (immutable-string-append
+   "bytes converter ran out of space even though no destination bytestring was"
+   " given to the converter, which should be impossible"))
+
+(define (immutable-bytes-convert-end converter)
+  (define-values (output-bstr status) (bytes-convert-end converter))
+  (unless (equal? status 'complete)
+    (raise-arguments-error 'immutable-bytes-convert-end
+                           bytes-convert-end-error-message
+                           "converter" converter
+                           "status" status
+                           "converter result bytes" output-bstr))
+  (bytes->immutable-bytes output-bstr))
+
+;; TODO(https://github.com/racket/racket/issues/2670): remove this wrapper since
+;;   it's only needed in order to lie about bytes-utf-8-index's arity.
+(define (immutable-bytes-utf-8-index bstr
+                                     pos
+                                     [err-byte #f]
+                                     [start 0]
+                                     [end (immutable-bytes-length bstr)])
+  (bytes-utf-8-index bstr pos err-byte start end))
--- private/immutable-bytes.scrbl
+++ private/immutable-bytes.scrbl
@@ -2,14 +2,9 @@
 
 @(require (for-label racket/base
                      racket/contract/base
-                     rebellion/binary/immutable-bytes)
-          (submod rebellion/private/scribble-evaluator-factory doc)
-          scribble/example)
-
-@(define make-evaluator
-   (make-module-sharing-evaluator-factory
-    #:public (list 'rebellion/binary/immutable-bytes)
-    #:private (list 'racket/base)))
+                     racket/math
+                     rebellion/binary/immutable-bytes
+                     rebellion/immutable-string))
 
 @title{Immutable Bytestrings}
 @defmodule[rebellion/binary/immutable-bytes]
@@ -22,3 +17,120 @@ always accept and return immutable bytestrings.
 @defproc[(immutable-bytes? [v any/c]) boolean?]{
  A predicate for @tech{immutable bytestrings}. Implies @racket[bytes?] and
  @racket[immutable?].}
+
+@defproc[(make-immutable-bytes [size natural?] [b byte?]) immutable-bytes?]
+@defproc[(immutable-bytes [b byte?] ...) immutable-bytes?]
+@defproc[(immutable-bytes-length [bstr immutable-bytes?]) natural?]
+@defproc[(immutable-bytes-ref [bstr immutable-bytes?] [pos natural?]) byte?]
+
+@defproc[(immutable-subbytes [bstr immutable-bytes?]
+                             [start natural?]
+                             [end natural? (immutable-bytes-length bstr)])
+         immutable-bytes?]
+
+@defproc[(immutable-bytes-append [bstr immutable-bytes?] ...) immutable-bytes?]
+@defproc[(immutable-bytes->list [bstr immutable-bytes?]) (listof byte?)]
+@defproc[(list->immutable-bytes [lst (listof byte?)]) immutable-bytes?]
+
+@defproc[(immutable-bytes=? [bstr1 immutable-bytes?]
+                            [bstr2 immutable-bytes?] ...)
+         boolean?]
+
+@defproc[(immutable-bytes<? [bstr1 immutable-bytes?]
+                            [bstr2 immutable-bytes?] ...)
+         boolean?]
+
+@defproc[(immutable-bytes>? [bstr1 immutable-bytes?]
+                            [bstr2 immutable-bytes?] ...)
+         boolean?]
+
+@defproc[(immutable-bytes->string/utf-8
+          [bstr immutable-bytes?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         immutable-string?]
+
+@defproc[(immutable-bytes->string/locale
+          [bstr immutable-bytes?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         immutable-string?]
+
+@defproc[(immutable-bytes->string/latin-1
+          [bstr immutable-bytes?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         immutable-string?]
+
+@defproc[(immutable-string->bytes/utf-8
+          [str immutable-string?]
+          [err-byte (or/c byte? #f)]
+          [start natural? 0]
+          [end (immutable-string-length str)])
+         immutable-bytes?]
+
+@defproc[(immutable-string->bytes/locale
+          [str immutable-string?]
+          [err-byte (or/c byte? #f)]
+          [start natural? 0]
+          [end (immutable-string-length str)])
+         immutable-bytes?]
+
+@defproc[(immutable-string->bytes/latin-1
+          [str immutable-string?]
+          [err-byte (or/c byte? #f)]
+          [start natural? 0]
+          [end (immutable-string-length str)])
+         immutable-bytes?]
+
+@defproc[(immutable-string-utf-8-length
+          [str immutable-string?]
+          [start natural? 0]
+          [end natural? (immutable-string-length str)])
+         natural?]
+
+@defproc[(immutable-bytes-utf-8-length
+          [bstr immutable-bytes?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         (or/c natural? #f)]
+
+@defproc[(immutable-bytes-utf-8-ref
+          [bstr immutable-bytes?]
+          [pos natural?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         (or/c char? #f)]
+
+@defproc[(immutable-bytes-utf-8-index
+          [bstr immutable-bytes?]
+          [pos natural?]
+          [err-char (or/c char? #f)]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length bstr)])
+         (or/c natural? #f)]
+
+@defproc[(immutable-bytes-convert
+          [converter bytes-converter?]
+          [source immutable-bytes?]
+          [start natural? 0]
+          [end natural? (immutable-bytes-length source)])
+         (values immutable-bytes?
+                 natural?
+                 (or/c 'complete 'continues 'aborts 'error))]
+
+@defproc[(immutable-bytes-convert-end [converter bytes-converter?])
+         immutable-bytes?]
+
+@defproc[(immutable-bytes-append* [bstr immutable-bytes?] ...
+                                  [bstrs (listof immutable-bytes?)])
+         immutable-bytes?]
+
+@defproc[(immutable-bytes-join [bstrs (listof immutable-bytes?)]
+                               [sep immutable-bytes?])
+         immutable-bytes?]
