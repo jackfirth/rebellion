68211a226940c6a186fc20bd6ff6c2d164c5ed95
Jack Firth <jackhfirth@gmail.com>
2019-06-29 16:12:50 -0700

    Implement define-wrapper-type

--- private/wrapper-type.rkt
+++ private/wrapper-type.rkt
@@ -27,12 +27,19 @@
    (-> uninitialized-wrapper-descriptor?
        (listof (cons/c struct-type-property? any/c)))]))
 
-(require racket/struct
+(require (for-syntax racket/base
+                     racket/syntax)
+         racket/struct
          rebellion/generative-token
          rebellion/symbol
          rebellion/type/record
          rebellion/type/tuple
          syntax/parse/define)
+
+(module+ test
+  (require (submod "..")
+           racket/format
+           rackunit))
 
 ;@------------------------------------------------------------------------------
 
@@ -95,12 +102,15 @@
                 #:predicate-name (wrapper-type-predicate-name type)
                 #:constructor-name (wrapper-type-constructor-name type)))
   (define (tuple-impl-prop-maker tuple-impl)
+    (define tuple-impl-accessor (tuple-descriptor-accessor tuple-impl))
+    (define (accessor this) (tuple-impl-accessor this 0))
+    (define accessor-name (wrapper-type-accessor-name type))
     (prop-maker
-      (uninitialized-wrapper-descriptor
-       #:type type
-       #:predicate (tuple-descriptor-predicate tuple-impl)
-       #:constructor (tuple-descriptor-constructor tuple-impl)
-       #:accessor (procedure-rename accessor (tuple-type-accessor-name type)))))
+     (uninitialized-wrapper-descriptor
+      #:type type
+      #:predicate (tuple-descriptor-predicate tuple-impl)
+      #:constructor (tuple-descriptor-constructor tuple-impl)
+      #:accessor (procedure-rename accessor accessor-name))))
   (define tuple-impl
     (make-tuple-implementation tuple-impl-type
                                #:inspector inspector
@@ -111,7 +121,7 @@
    #:type type
    #:predicate (tuple-descriptor-predicate tuple-impl)
    #:constructor (tuple-descriptor-constructor tuple-impl)
-   #:accessor (procedure-rename accessor (tuple-type-accessor-name type))))
+   #:accessor (procedure-rename accessor (wrapper-type-accessor-name type))))
 
 (define (make-delegating-equal+hash delegate-extractor)
   (define token (make-generative-token))
@@ -134,4 +144,39 @@
   (list (cons prop:equal+hash equal+hash)
         (cons prop:custom-write custom-write)))
 
-(define-simple-macro (define-wrapper-type id:id) (begin))
+(define-simple-macro
+  (define-wrapper-type id:id
+    (~alt (~optional (~seq #:predicate-name predicate-name:id)
+                     #:defaults ([predicate-name (format-id #'id "~a?" #'id)])
+                     #:name "#:predicate-name option")
+          (~optional (~seq #:constructor-name constructor-name:id)
+                     #:defaults ([constructor-name #'id])
+                     #:name "#:constructor-name option")
+          (~optional (~seq #:accessor-name accessor-name:id)
+                     #:defaults ([accessor-name
+                                  (format-id #'id "~a-value" #'id)])
+                     #:name "#:accessor-name option")
+          (~optional (~seq #:property-maker prop-maker:expr)
+                     #:defaults ([prop-maker #'make-default-wrapper-properties])
+                     #:name "#:property-maker option"))
+    ...)
+  (begin
+    (define type
+      (wrapper-type 'id
+                    #:predicate-name 'predicate-name
+                    #:constructor-name 'constructor-name
+                    #:accessor-name 'accessor-name))
+    (define descriptor
+      (make-wrapper-implementation type #:property-maker prop-maker))
+    (define predicate-name (wrapper-descriptor-predicate descriptor))
+    (define constructor-name (wrapper-descriptor-constructor descriptor))
+    (define accessor-name (wrapper-descriptor-accessor descriptor))))
+
+(module+ test
+  (test-case "integration-test"
+    (define-wrapper-type seconds)
+    (check-equal? (seconds 10) (seconds 10))
+    (check-not-equal? (seconds 10) (seconds 25))
+    (check-equal? (seconds-value (seconds 10)) 10)
+    (check-pred seconds? (seconds 10))
+    (check-equal? (~v (seconds 10)) "(seconds 10)")))
