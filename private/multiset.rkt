de3e53aab6731582066df10208c9f44d6f45aadf
Jack Firth <jackhfirth@gmail.com>
2019-07-13 17:08:44 -0600

    Add multiset-add and multiset iterations and comprehensions

--- private/multiset.rkt
+++ private/multiset.rkt
@@ -3,9 +3,12 @@
 (require racket/contract/base)
 
 (provide
+ for/multiset
+ for*/multiset
  (contract-out
   [multiset (-> any/c ... multiset?)]
   [multiset? (-> any/c boolean?)]
+  [multiset-add (-> multiset? any/c multiset?)]
   [multiset-contains? (-> multiset? any/c boolean?)]
   [multiset-frequency (-> multiset? any/c natural?)]
   [multiset-frequencies
@@ -14,11 +17,16 @@
   [multiset-unique-elements (-> multiset? immutable-set?)]
   [multiset->list (-> multiset? list?)]
   [list->multiset (-> list? multiset?)]
-  [empty-multiset multiset?]))
+  [empty-multiset multiset?]
+  [in-multiset (-> multiset? sequence?)]
+  [into-multiset reducer?]))
 
-(require racket/math
+(require (for-syntax racket/base)
+         racket/math
          racket/set
+         racket/stream
          racket/struct
+         rebellion/streaming/reducer
          rebellion/type/tuple)
 
 (module+ test
@@ -36,6 +44,11 @@
 
 ;@------------------------------------------------------------------------------
 
+(define (in-multiset set)
+  (for*/stream ([(v count) (in-immutable-hash (multiset-frequencies set))]
+                [_ (in-range count)])
+    v))
+
 (define (make-multiset-props descriptor)
   (define name (tuple-type-name (tuple-descriptor-type descriptor)))
   (define accessor (tuple-descriptor-accessor descriptor))
@@ -45,7 +58,8 @@
      (λ (_) name)
      (λ (this) (frequency-hash->list (accessor this 1)))))
   (list (cons prop:equal+hash equal+hash)
-        (cons prop:custom-write custom-write)))
+        (cons prop:custom-write custom-write)
+        (cons prop:sequence in-multiset)))
 
 (define-tuple-type multiset (size frequencies)
   #:property-maker make-multiset-props
@@ -54,12 +68,20 @@
 (define empty-hash (hash))
 
 (define (multiset . vs)
-  (for/fold ([h empty-hash]
-             [size 0]
-             #:result (constructor:multiset size h))
-            ([v (in-list vs)])
-    (values (hash-set h v (add1 (hash-ref h v 0)))
-            (add1 size))))
+  (for/multiset ([v (in-list vs)])
+    v))
+
+(define empty-multiset (constructor:multiset 0 empty-hash))
+
+(define (multiset-add set v)
+  (constructor:multiset (add1 (multiset-size set))
+                        (hash-update (multiset-frequencies set) v add1 0)))
+
+(define into-multiset
+  (make-fold-reducer multiset-add empty-multiset #:name 'into-multiset))
+
+(define-syntaxes (for/multiset for*/multiset)
+  (make-reducer-based-for-comprehensions #'into-multiset))
 
 (define (multiset-frequency set elem)
   (hash-ref (multiset-frequencies set) elem 0))
@@ -80,17 +102,22 @@
               [_ (in-range freq)])
     elem))
 
-(define empty-multiset (multiset))
-
 (module+ test
   (define letters (multiset 'a 'b 'b 'b 'c 'c 'd))
-  (check-equal? (multiset-frequencies letters) (hash 'a 1 'b 3 'c 2 'd 1))
-  (check-equal? (multiset-frequency letters 'b) 3)
-  (check-equal? (multiset-frequency letters 'foo) 0)
-  (check-equal? (multiset-size letters) 7)
-  (check-equal? (multiset-unique-elements letters) (set 'a 'b 'c 'd))
-  (check-true (multiset-contains? letters 'a))
-  (check-true (multiset-contains? letters 'b))
-  (check-false (multiset-contains? letters 'foo))
-  (check-equal? (list->multiset (multiset->list letters)) letters)
-  (check-equal? (length (multiset->list letters)) 7))
+  (test-case "queries"
+    (check-equal? (multiset-frequencies letters) (hash 'a 1 'b 3 'c 2 'd 1))
+    (check-equal? (multiset-frequency letters 'b) 3)
+    (check-equal? (multiset-frequency letters 'foo) 0)
+    (check-equal? (multiset-size letters) 7)
+    (check-equal? (multiset-unique-elements letters) (set 'a 'b 'c 'd))
+    (check-true (multiset-contains? letters 'a))
+    (check-true (multiset-contains? letters 'b))
+    (check-false (multiset-contains? letters 'foo)))
+  (test-case "conversion"
+    (check-equal? (list->multiset (multiset->list letters)) letters)
+    (check-equal? (length (multiset->list letters)) 7))
+  (test-case "iteration"
+    (check-equal? (for/multiset ([char (in-string "hello")]) char)
+                  (multiset #\h #\e #\l #\l #\o))
+    (check-equal? (for/set ([letter (in-multiset letters)]) letter)
+                  (set 'a 'b 'c 'd))))
