a66fc58d44c15815ce0a66098e241acf0bca533d
Jack Firth <jackhfirth@gmail.com>
2019-10-17 00:54:39 -0700

    Add deduplicating transducer
    Closes #211.

---
+++ private/transducer-deduplicating-test.rkt
@@ -0,0 +1,15 @@
+#lang racket/base
+
+(module+ test
+  (require rackunit
+           rebellion/streaming/reducer
+           rebellion/streaming/transducer))
+
+;@------------------------------------------------------------------------------
+
+(module+ test
+  (test-case "deduplicating"
+    (check-equal? (transduce "hello world" deduplicating #:into into-string)
+                  "helo wrd")
+    (check-equal? (transduce "zzzzzzzzzzz" deduplicating #:into into-string)
+                  "z")))
