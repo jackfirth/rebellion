ffb663716522ab1000def96681a3eb6d4f62d410
Jack Firth <jackhfirth@gmail.com>
2019-10-19 16:40:50 -0700

    Remove key caching to simplify design.
    Also, discussion with @rocketnia revealed flaw in claim that this approach is O(n log k). Revised the comments about asymptotic performance.

--- private/transducer-sorting.rkt
+++ private/transducer-sorting.rkt
@@ -18,53 +18,41 @@
 
 ;@------------------------------------------------------------------------------
 
-;; The implementation of sorting transducers is lazily efficient; if only the
-;; first k elements of a sorted sequence of n elements are consumed then the
-;; amount of work done is O(n log k), rather than the O(n log n) cost of a full
-;; sort. This property is achieved by using partially sorted trees. A partially
-;; sorted tree is like a binary sort tree, but instead of being fully sorted
-;; with an element, a subtree of smaller elements, and a subtree of greater
-;; elements, elements larger than the pivot are left unsorted. Elements smaller
+;; The implementation of sorting transducers is lazily efficient. If only the
+;; k smallest elements of a sequence of n elements are demanded, then the
+;; remaining elements are left unsorted. This is faster than the O(n log n) cost
+;; of a full sort, but I'm unsure what the exact running time of the current
+;; implementation is.
+
+;; A (partially-sorted-tree/c E) is a partially sorted tree of elements of
+;; type E. A partially sorted tree is like a binary tree, but instead of being
+;; fully sorted with an element, a subtree of smaller elements, and a subtree of
+;; greater elements, the greater elements are left unsorted. Elements smaller
 ;; than the pivot it are recursively partially sorted. This representation
 ;; allows efficient lazy traversal of the tree from least to greatest, because
 ;; the lesser subtree contains all elements smaller than the pivot and can be
 ;; iterated without sorting elements larger than the pivot.
-
-;; A (partially-sorted-tree/c K E) is a partially sorted tree of elements of
-;; type E that are compared by their keys of type K.
 (define-record-type partially-sorted-tree
   ;; E
   ;; The tree's pivot element, used to partition other elements
   (pivot-element
 
-   ;; (option/c K)
-   ;; The pivot's key, as computed by a key function. Starts absent to allow
-   ;; lazy key construction.
-   pivot-key
-
-   ;; (partially-sorted-tree/c K E)
-   ;; A subtree containing all elements that are smaller than this tree's pivot.
-   ;; Every element in this subtree has its key computed.
+   ;; (or/c (partially-sorted-tree/c E) empty-tree?)
+   ;; A subtree containing all elements that are smaller than this tree's pivot,
+   ;; or the empty tree if there are no such elements.
    lesser-subtree
 
    ;; (listof E)
    ;; Reverse-ordered elements equivalent to pivot. Equivalent elements are
    ;; stored in reverse order to allow efficient insertion into the head of a
    ;; list. However, this requires reversing the list before iterating
-   ;; equivalent elements in order to preserve encounter order. Note that keys
-   ;; for these elements were computed, but then discarded because there's no
-   ;; need to compare them to anything anymore.
+   ;; equivalent elements in order to preserve encounter order.
    equivalent-stack
 
-   ;; (listof (entry/c K E))
+   ;; (listof E)
    ;; Reverse-ordered elements greater than pivot. Stored in reverse order for
-   ;; the same reasons as for equivalent-stack. Keys are stored alongside
-   ;; elements because further sorting is required and keys should only be
-   ;; computed once.
+   ;; the same reasons as for equivalent-stack.
    greater-stack))
-
-;; TODO: finish implementing this so I can confirm that this approach actually
-;; works.
 
 (define-singleton-type empty-tree)
 
@@ -74,6 +62,11 @@
   #f)
 
 (define (sorting [comparator real<=>] #:key [key-function values])
+  ;; TODO(https://github.com/jackfirth/): handle key function more efficiently
+  ;;   by caching keys, and avoid computing them altogether when possible (e.g.
+  ;;   singleton sequence).
+  (define keyed-comparator (comparator-map real<=> key-function))
+  
   (define (start)
     (variant #:consume empty-tree))
   (define (consume state element)
@@ -82,7 +75,6 @@
         [(empty-tree? state)
          (partially-sorted-tree
           #:pivot-element element
-          #:pivot-key absent
           #:lesser-subtree empty-tree
           #:equivalent-stack empty-list
           #:greater-stack empty-list)]
@@ -94,7 +86,7 @@
       [else (variant #:half-closed-emit tree)]))
   (define (half-closed-emit tree)
     (define trimming
-      (partially-sorted-tree-trim-minimum tree comparator key-function))
+      (partially-sorted-tree-trim-minimum tree keyed-comparator))
     (define minimum (tree-trimming-minimum-leaf trimming))
     (define next-tree (tree-trimming-leftover-tree trimming))
     (define next-state
