255a0b64d018a0eb86f558a9943ec81d0d4b6c6c
Jack Firth <jackhfirth@gmail.com>
2019-11-06 22:03:10 -0800

    Change test case naming for batching transducer tests

--- private/transducer-batching-test.rkt
+++ private/transducer-batching-test.rkt
@@ -12,7 +12,7 @@
 
 (module+ test
   (test-case (name-string batching)
-    (test-case "should-emit-last-batch-on-half-close"
+    (test-case "should emit last batch on half close"
       (define trans (materializing (batching (reducer-limit into-list 4))))
       (check-equal? (transduce (in-range 10) trans #:into into-list)
                     (list start-event
@@ -32,7 +32,7 @@
                           (half-closed-emit-event (list 8 9))
                           finish-event)))
 
-    (test-case "should-not-emit-empty-batches"
+    (test-case "should not emit empty batches"
       (define trans (materializing (batching (reducer-limit into-list 5))))
       (check-equal? (transduce (in-range 10) trans #:into into-list)
                     (list start-event
@@ -51,7 +51,7 @@
                           half-close-event
                           finish-event)))
 
-    (test-case "should-emit-one-batch-if-reducer-never-finishes"
+    (test-case "should emit one batch if reducer never finishes"
       (define trans (materializing (batching into-list)))
       (check-equal? (transduce (in-range 10) trans #:into into-list)
                     (list start-event
@@ -69,7 +69,7 @@
                           (half-closed-emit-event (list 0 1 2 3 4 5 6 7 8 9))
                           finish-event)))
 
-    (test-case "should-emit-nothing-if-upstream-empty"
+    (test-case "should emit nothing if upstream empty"
       (define trans (materializing (batching into-list)))
       (check-equal? (transduce empty-list trans #:into into-list)
                     (list start-event
