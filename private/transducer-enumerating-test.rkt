a2ba9f709fb6b486627aa79bb167c1757058f4d6
Jack Firth <jackhfirth@gmail.com>
2019-11-06 22:10:50 -0800

    Improve tests for enumerating transducer

--- private/transducer-enumerating-test.rkt
+++ private/transducer-enumerating-test.rkt
@@ -1,16 +1,32 @@
 #lang racket/base
 
 (module+ test
-  (require (submod "..")
-           rackunit
+  (require rackunit
            rebellion/collection/list
-           rebellion/streaming/transducer))
+           rebellion/private/static-name
+           rebellion/streaming/transducer
+           rebellion/streaming/transducer/testing))
 
 ;@------------------------------------------------------------------------------
 
 (module+ test
-  (test-case "enumerating"
-    (check-equal? (transduce "cat" enumerating #:into into-list)
-                  (list (enumerated #:element #\c #:position 0)
-                        (enumerated #:element #\a #:position 1)
-                        (enumerated #:element #\t #:position 2)))))
+  (test-case (name-string enumerating)
+    (define inputs "cat")
+    (define expected
+      (list (enumerated #:element #\c #:position 0)
+            (enumerated #:element #\a #:position 1)
+            (enumerated #:element #\t #:position 2)))
+    (define expected-events
+      (list start-event
+            (consume-event #\c)
+            (emit-event (enumerated #:element #\c #:position 0))
+            (consume-event #\a)
+            (emit-event (enumerated #:element #\a #:position 1))
+            (consume-event #\t)
+            (emit-event (enumerated #:element #\t #:position 2))
+            half-close-event
+            finish-event))
+    (check-equal? (transduce inputs enumerating #:into into-list) expected)
+    (define actual-events
+      (transduce inputs (materializing enumerating) #:into into-list))
+    (check-equal? actual-events expected-events)))
