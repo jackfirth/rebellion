7006479facb9f4bd7bc3b39df0897620fdcbb3c3
Jack Firth <jackhfirth@gmail.com>
2019-11-19 23:38:56 -0800

    Expose transducer-pipe from r/s/transducer/composition

--- streaming/transducer.rkt
+++ streaming/transducer.rkt
@@ -1,8 +1,8 @@
 #lang reprovide
 rebellion/streaming/transducer/base
+rebellion/streaming/transducer/composition
 rebellion/private/transducer
 rebellion/private/transducer-batching
 rebellion/private/transducer-deduplicating
 rebellion/private/transducer-enumerating
-rebellion/private/transducer-pipe
 rebellion/private/transducer-sorting
