f4f16e6153bae78fbb0b82346afae2ea69dac87e
Jack Firth <jackhfirth@gmail.com>
2020-01-02 19:34:49 -0800

    Revert "Implement pattern matching for enum types"
    This reverts commit d436a8c868084b74400fa6f7ea8e923ddc790980.

--- private/enum-type.rkt
+++ private/enum-type.rkt
@@ -5,7 +5,6 @@
 (require (for-syntax racket/base
                      racket/sequence
                      racket/syntax)
-         racket/match
          rebellion/collection/keyset/low-dependency
          rebellion/type/enum/base
          rebellion/type/enum/descriptor
@@ -52,17 +51,13 @@
                 #:defaults ([prop-maker #'default-enum-properties])
                 #:name "#:property-maker option"))
     ...)
-  #:with (local-case-id ...) (generate-temporaries #'(cases.id ...))
   (begin
     (define type (enum-type 'id cases.names #:predicate-name 'predicate-name))
     (define descriptor
       (make-enum-implementation type #:property-maker prop-maker))
     (define predicate-name (enum-descriptor-predicate descriptor))
     (define selector (enum-descriptor-selector descriptor))
-    (define local-case-id (selector cases.index)) ...
-    (define-match-expander cases.id
-      (syntax-parser [_:id #'(== cases.id)])
-      (make-rename-transformer #'local-case-id))
+    (define cases.id (selector cases.index))
     ...))
 
 (module+ test
@@ -76,7 +71,6 @@
     (check-false (compass-direction? 42))
     (check-equal? (object-name compass-direction?) (name compass-direction?))
     (check-equal? (object-name north) (name north))
-    (check-match north north)
     (check-equal? (~a south) "#<compass-direction:south>")
     (check-equal? (~v east) "#<compass-direction:east>")
     (check-equal? (~s west) "#<compass-direction:west>")))
