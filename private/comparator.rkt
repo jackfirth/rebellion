f658d01991d9689464a9cb763eb9f19e73988b24
Jack Firth <jackhfirth@gmail.com>
2020-01-15 22:17:47 -0800

    Add comparator contracts
    Closes #378.

--- private/comparator.rkt
+++ private/comparator.rkt
@@ -20,25 +20,33 @@
   [lesser comparison?]
   [greater comparison?]
   [equivalent comparison?]
-  [real<=> comparator?]
-  [string<=> comparator?]
+  [real<=> (comparator/c comparable-real?)]
+  [string<=> (comparator/c immutable-string?)]
+  [comparable-real? predicate/c]
   [comparator-impersonate
    (->* (comparator?)
         (#:operand-guard (or/c (-> any/c any/c) #f)
          #:properties impersonator-property-hash/c
          #:comparison-marks (and/c hash? immutable?)
          #:chaperone? boolean?)
-        comparator?)]))
+        comparator?)]
+  [comparator/c (-> contract? contract?)]))
 
-(require rebellion/base/immutable-string
+(require racket/contract/combinator
+         rebellion/base/immutable-string
          rebellion/base/symbol
+         rebellion/private/contract-projection
          rebellion/private/impersonation
          rebellion/private/static-name
+         rebellion/private/strict-cond
          rebellion/type/reference
          rebellion/type/singleton)
 
 (module+ test
   (require (submod "..")
+           racket/contract/parametric
+           racket/contract/region
+           racket/function
            rackunit))
 
 ;@------------------------------------------------------------------------------
@@ -72,9 +80,12 @@
   (define (wrapped-func left right) (func right left))
   (make-comparator wrapped-func))
 
+(define (comparable-real? v) (and (real? v) (not (equal? v +nan.0))))
+
 (define/name real<=>
   (make-comparator
-   (λ (x y) (cond [(< x y) lesser] [(= x y) equivalent] [else greater]))
+   (λ (x y)
+     (strict-cond [(< x y) lesser] [(= x y) equivalent] [(> x y) greater]))
    #:name enclosing-variable-name))
 
 (define/name string<=>
@@ -89,11 +100,42 @@
   (test-case (name-string real<=>)
     (check-equal? (compare real<=> 4 5.2) lesser)
     (check-equal? (compare real<=> 0 -7) greater)
-    (check-equal? (compare real<=> 3 3) equivalent))
+    (check-equal? (compare real<=> 3 3) equivalent)
+
+    (test-case "should ignore exactness"
+      (check-equal? (compare real<=> 5 5.0) equivalent)
+      (check-equal? (compare real<=> 5.0 5) equivalent))
+
+    (test-case "should treat all variants of zero as equivalent"
+      (check-equal? (compare real<=> 0 0.0) equivalent)
+      (check-equal? (compare real<=> 0 -0.0) equivalent)
+      (check-equal? (compare real<=> 0.0 -0.0) equivalent))
+
+    (test-case "should not allow comparing values to the not-a-number constants"
+      (check-exn exn:fail:contract:blame? (λ () (compare real<=> 3 +nan.0)))
+      (check-exn exn:fail:contract:blame? (λ () (compare real<=> +nan.0 3)))
+      (check-exn exn:fail:contract:blame? (λ () (compare real<=> 3 +nan.f)))
+      (check-exn exn:fail:contract:blame? (λ () (compare real<=> +nan.f 3))))
+
+    (test-case "should allow comparisons to infinities"
+      (check-equal? (compare real<=> 4 +inf.0) lesser)
+      (check-equal? (compare real<=> 4 +inf.f) lesser)
+      (check-equal? (compare real<=> 4 -inf.0) greater)
+      (check-equal? (compare real<=> 4 -inf.f) greater)
+      (check-equal? (compare real<=> -inf.0 +inf.0) lesser)
+      (check-equal? (compare real<=> -inf.f +inf.f) lesser))
+
+    (test-case "should treat same-signed infinities as equivalent"
+      (check-equal? (compare real<=> +inf.0 +inf.f) equivalent)
+      (check-equal? (compare real<=> +inf.f +inf.0) equivalent)
+      (check-equal? (compare real<=> -inf.0 -inf.f) equivalent)
+      (check-equal? (compare real<=> -inf.f -inf.0) equivalent)))
+
   (test-case (name-string string<=>)
     (check-equal? (compare string<=> "apple" "banana") lesser)
     (check-equal? (compare string<=> "apple" "aardvark") greater)
     (check-equal? (compare string<=> "apple" "apple") equivalent))
+  
   (test-case (name-string comparator-reverse)
     (define reversed (comparator-reverse real<=>))
     (check-equal? (compare reversed 1 2) greater)
@@ -138,3 +180,74 @@
     (check-true (impersonator-of? chaperoned real<=>))
     (check-false (chaperone-of? real<=> chaperoned))
     (check-false (impersonator-of? real<=> chaperoned))))
+
+(define/name (comparator/c operand-contract*)
+  (define operand-contract
+    (coerce-contract enclosing-function-name operand-contract*))
+  (define contract-name
+    (build-compound-type-name enclosing-function-name operand-contract))
+  (define operand-projection (contract-late-neg-projection operand-contract))
+  (define chaperone? (chaperone-contract? operand-contract))
+  (define (projection blame)
+    (define operand-blame (blame-add-context blame "an operand of" #:swap? #t))
+    (define late-neg-operand-guard (operand-projection operand-blame))
+    (λ (v missing-party)
+      (assert-satisfies v comparator? blame #:missing-party missing-party)
+      (define props
+        (hash impersonator-prop:contracted the-contract
+              impersonator-prop:blame (cons blame missing-party)))
+      (define (operand-guard v) (late-neg-operand-guard v missing-party))
+      (comparator-impersonate v
+                              #:operand-guard operand-guard
+                              #:chaperone? chaperone?
+                              #:properties props)))
+  (define the-contract
+    ((if chaperone? make-chaperone-contract make-contract)
+     #:name contract-name
+     #:first-order comparator?
+     #:late-neg-projection projection))
+  the-contract)
+
+(module+ test
+  (test-case (name-string comparator/c)
+
+    (define digit<=>
+      (make-comparator
+       (λ (x y)
+         (strict-cond
+           [(equal? x y) equivalent]
+           [(< x y) lesser]
+           [(> x y) greater]))
+       #:name (name digit<=>)))
+
+    (define digit/c (integer-in 0 9))
+
+    (test-case "should make chaperones for non-impersonator operand contracts"
+      (check-pred chaperone-contract? (comparator/c any/c))
+      (check-pred chaperone-contract? (comparator/c string?))
+      (check-pred chaperone-contract? (comparator/c (-> any/c any/c)))
+      (check-pred (negate chaperone-contract?) (comparator/c (new-∀/c))))
+
+    (test-case "should only enforce comparator? predicate in first order checks"
+      (check-exn exn:fail:contract:blame?
+                 (λ () (invariant-assertion (comparator/c any/c) 42)))
+      (check-not-exn (λ () (invariant-assertion (comparator/c any/c) digit<=>)))
+      (check-not-exn
+       (λ () (invariant-assertion (comparator/c digit/c) digit<=>)))
+      (check-not-exn
+       (λ () (invariant-assertion (comparator/c string?) digit<=>))))
+    
+    (test-case "should enforce flat operand contracts"
+      (define/contract contracted (comparator/c digit/c) digit<=>)
+      (check-not-exn (λ () (compare contracted 5 5)))
+      (check-exn exn:fail:contract:blame? (λ () (compare contracted 5 10)))
+      (check-exn exn:fail:contract:blame? (λ () (compare contracted -1 5)))
+      (check-exn exn:fail:contract:blame? (λ () (compare contracted 5 "foo")))
+      (check-exn exn:fail:contract:blame? (λ () (compare contracted "foo" 5))))
+
+    (test-case "should add contract system impersonator properties"
+      (define the-contract (comparator/c digit/c))
+      (define/contract contracted the-contract digit<=>)
+      (check-pred has-contract? contracted)
+      (check-equal? (value-contract contracted) the-contract)
+      (check-pred has-blame? contracted))))
