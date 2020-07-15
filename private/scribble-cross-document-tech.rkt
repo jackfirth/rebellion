936b2afb62994f7b1594b8d0724703db90b27549
Jack Firth <jackhfirth@gmail.com>
2020-07-15 03:30:27 -0700

    Improve cross-document tech utils

--- private/scribble-cross-document-tech.rkt
+++ private/scribble-cross-document-tech.rkt
@@ -5,13 +5,14 @@
   (require racket/contract/base)
 
   (provide
-    (contract-out
-      [tech/guide
-        ;; Similar to @tech{....}, but creates a link to a `deftech` in The Racket Guide
-        (->* () () #:rest (listof pre-content?) element?)]
-      [tech/reference
-        ;; Links to a `deftech` in The Racket Reference
-        (->* () () #:rest (listof pre-content?) element?)]))
+   (contract-out
+    ;; Similar to @tech{....}, but creates a link to a `deftech` in The Racket
+    ;; Guide.
+    [tech/guide cross-document-tech-function/c]
+    ;; Links to a `deftech` in The Racket Reference.
+    [tech/reference cross-document-tech-function/c]
+    ;; Links to a `deftech` in the `syntax/` libraries.
+    [syntax-tech cross-document-tech-function/c]))
 
   (require rebellion/base/immutable-string
            scribble/base
@@ -19,8 +20,31 @@
            scribble/decode
            scribble/manual)
 
-  (define (tech/guide . text)
-    (apply tech #:doc '(lib "scribblings/guide/guide.scrbl") text))
+  ;@----------------------------------------------------------------------------
 
-  (define (tech/reference . text)
-    (apply tech #:doc '(lib "scribblings/reference/reference.scrbl") text)))
+  (define cross-document-tech-function/c
+    (->* ()
+         (#:key (or/c string? #f) #:normalize? boolean?)
+         #:rest (listof pre-content?)
+         element?))
+
+  (define (tech/guide #:key [key #f] #:normalize? [normalize? #t] . text)
+    (apply tech
+           #:doc '(lib "scribblings/guide/guide.scrbl")
+           #:key key
+           #:normalize? normalize?
+           text))
+
+  (define (tech/reference #:key [key #f] #:normalize? [normalize? #t] . text)
+    (apply tech
+           #:doc '(lib "scribblings/reference/reference.scrbl")
+           #:key key
+           #:normalize? normalize?
+           text))
+
+  (define (syntax-tech #:key [key #f] #:normalize? [normalize? #t] . text)
+    (apply tech
+           #:doc '(lib "syntax/scribblings/syntax.scrbl")
+           #:key key
+           #:normalize? normalize?
+           text)))
