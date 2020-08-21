6921cffb1014932921bd7f190060bbd6d959a44c
Jack Firth <jackhfirth@gmail.com>
2020-08-20 19:47:50 -0700

    Implement wrapper type bindings

---
+++ private/wrapper-type-binding.rkt
@@ -0,0 +1,73 @@
+#lang racket/base
+
+(require racket/contract/base)
+
+(provide
+ wrapper-id
+ (contract-out
+  [wrapper-binding? predicate/c]
+  [wrapper-binding-type (-> wrapper-binding? wrapper-type?)]
+  [wrapper-binding-descriptor (-> wrapper-binding? identifier?)]
+  [wrapper-binding-predicate (-> wrapper-binding? identifier?)]
+  [wrapper-binding-constructor (-> wrapper-binding? identifier?)]
+  [wrapper-binding-accessor (-> wrapper-binding? identifier?)]))
+
+(module+ private-constructor
+  (provide
+   (contract-out
+    [wrapper-binding
+     (-> #:type wrapper-type?
+         #:descriptor identifier?
+         #:predicate identifier?
+         #:constructor identifier?
+         #:accessor identifier?
+         #:pattern identifier?
+         #:macro (-> syntax? syntax?)
+         wrapper-binding?)])))
+
+(require (for-template racket/base
+                       racket/match)
+         racket/sequence
+         racket/syntax
+         rebellion/type/wrapper/base
+         syntax/parse)
+
+;@------------------------------------------------------------------------------
+
+(struct wrapper-binding
+  (type descriptor predicate constructor accessor pattern macro)
+  #:omit-define-syntaxes
+  #:constructor-name constructor:wrapper-binding
+
+  #:property prop:match-expander
+  (λ (this stx)
+    (define/with-syntax pattern (wrapper-binding-pattern this))
+    (syntax-parse stx #:track-literals
+      [(_ . body) (quasisyntax/loc stx (pattern . body))]))
+
+  #:property prop:procedure (λ (this stx) ((wrapper-binding-macro this) stx)))
+
+(define (wrapper-binding
+         #:type type
+         #:descriptor descriptor
+         #:predicate predicate
+         #:constructor constructor
+         #:accessor accessor
+         #:pattern pattern
+         #:macro macro)
+  (constructor:wrapper-binding
+   type descriptor predicate constructor accessor pattern macro))
+
+(define-syntax-class wrapper-id
+  #:attributes (type name descriptor predicate constructor accessor)
+
+  (pattern binding
+    #:declare binding
+    (static wrapper-binding? "a static wrapper-binding? value")
+    #:cut
+    #:attr type (wrapper-binding-type (attribute binding.value))
+    #:with name #`'#,(wrapper-type-name (attribute type))
+    #:with descriptor (wrapper-binding-descriptor (attribute binding.value))
+    #:with predicate (wrapper-binding-predicate (attribute binding.value))
+    #:with constructor (wrapper-binding-constructor (attribute binding.value))
+    #:with accessor (wrapper-binding-accessor (attribute binding.value))))
