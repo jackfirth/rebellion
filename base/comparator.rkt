2d3a7936ef6f1567acf8123b92b2ff5f1ee1508b
Jack Firth <jackhfirth@gmail.com>
2021-08-06 22:43:31 -0700

    Make `comparator-reverse` an involution
    By storing a pre-reversed form of the comparison function in the comparator struct, we can make `comparator-reverse` just swap some fields around instead of allocating a new procedure. This makes `comparator-reverse` an involution, meaning that `(comparator-reverse (comparator-reverse cmp))` is `equal?` to `cmp`. This property is important for sorted collections, since reversing an immutable sorted collection twice should produce a collection that's equal to the original.

--- base/comparator.rkt
+++ base/comparator.rkt
@@ -67,34 +67,52 @@
 ;@------------------------------------------------------------------------------
 
 
-(define-object-type comparator (function)
+(define-object-type comparator (function reverse-function reverse-name)
   #:constructor-name constructor:comparator)
+
 
 (define-singleton-type lesser)
 (define-singleton-type greater)
 (define-singleton-type equivalent)
 
-(define (comparison? v) (or (lesser? v) (greater? v) (equivalent? v)))
+
+(define (comparison? v)
+  (or (lesser? v) (greater? v) (equivalent? v)))
+
 
 (define (make-comparator function* #:name [name #false])
   (define function
     (if (equal? (procedure-arity function*) 2)
         function*
         (procedure-reduce-arity function* 2)))
-  (constructor:comparator #:function function #:name name))
+
+  (define (reverse-function left right)
+    (function right left))
+  
+  (constructor:comparator
+   #:function function #:reverse-function reverse-function #:reverse-name 'reversed #:name name))
+
 
 (define (compare comparator left right)
   ((comparator-function comparator) left right))
 
+
 (define (comparator-map comparator mapper #:name [name #false])
   (define func (comparator-function comparator))
-  (define (wrapped-func left right) (func (mapper left) (mapper right)))
+
+  (define (wrapped-func left right)
+    (func (mapper left) (mapper right)))
+
   (make-comparator wrapped-func #:name name))
 
+
 (define (comparator-reverse comparator)
-  (define func (comparator-function comparator))
-  (define (wrapped-func left right) (func right left))
-  (make-comparator wrapped-func))
+  (constructor:comparator
+   #:function (comparator-reverse-function comparator)
+   #:reverse-function (comparator-function comparator)
+   #:reverse-name (object-name comparator)
+   #:name (comparator-reverse-name comparator)))
+
 
 (define (comparator-chain #:name [name 'chained] . comparators)
   (make-comparator
@@ -106,8 +124,10 @@
      (or chain-result equivalent))
    #:name name))
 
+
 (define (has-duplicates? lst)
   (not (equal? (set-count (list->set lst)) (length lst))))
+
 
 (define/name (comparator-of-constants . ascending-constants)
   (when (has-duplicates? ascending-constants)
@@ -147,11 +167,13 @@
 
 (define (comparable-real? v) (and (real? v) (not (equal? v +nan.0))))
 
+
 (define/name real<=>
   (make-comparator
    (λ (x y)
      (strict-cond [(< x y) lesser] [(= x y) equivalent] [(> x y) greater]))
    #:name enclosing-variable-name))
+
 
 (define/name natural<=>
   (make-comparator
@@ -159,6 +181,7 @@
      (define difference (- x y))
      (strict-cond [(zero? difference) equivalent] [(positive? difference) greater] [else lesser]))
    #:name enclosing-variable-name))
+
 
 (define/name string<=>
   (make-comparator
@@ -168,10 +191,12 @@
            [else greater]))
    #:name enclosing-variable-name))
 
+
 (define/name char<=>
   (make-comparator
    (λ (c1 c2) (cond [(char<? c1 c2) lesser] [(equal? c1 c2) equivalent] [else greater]))
    #:name enclosing-variable-name))
+
 
 (define/name symbol<=>
   (make-comparator
@@ -183,10 +208,12 @@
        [else equivalent]))
    #:name enclosing-variable-name))
 
+
 (define/name interned-symbol<=>
   (make-comparator
    (λ (s1 s2) (cond [(eq? s1 s2) equivalent] [(symbol<? s1 s2) lesser] [else greater]))
    #:name enclosing-variable-name))
+
 
 (module+ test
   (test-case (name-string real<=>)
@@ -260,7 +287,8 @@
     (define reversed (comparator-reverse real<=>))
     (check-equal? (compare reversed 1 2) greater)
     (check-equal? (compare reversed 2 1) lesser)
-    (check-equal? (compare reversed 1 1) equivalent))
+    (check-equal? (compare reversed 1 1) equivalent)
+    (check-equal? (comparator-reverse reversed) real<=>))
 
   (test-case (name-string comparator-chain)
     (struct gemstone (type weight) #:transparent)
@@ -303,28 +331,41 @@
     (check-equal? (comparator-max real<=> 1 99 1) 99)
     (check-equal? (comparator-max real<=> 1 1 99) 99)))
 
+
 ;@------------------------------------------------------------------------------
 ;; Contracts
 
+
 (define ((comparator-function-guard argument-guard) left right)
   (values (argument-guard left) (argument-guard right)))
+
 
 (define (comparator-impersonate comparator
                                 #:operand-guard [guard #false]
                                 #:properties [properties (hash)]
                                 #:comparison-marks [marks (hash)]
                                 #:chaperone? [chaperone? (not guard)])
-  (define function (comparator-function comparator))
   (define impersonated-function
     (function-impersonate
-     function
+     (comparator-function comparator)
+     #:arguments-guard (and guard (comparator-function-guard guard))
+     #:application-marks marks
+     #:chaperone? chaperone?))
+  (define impersonated-reverse-function
+    (function-impersonate
+     (comparator-reverse-function comparator)
      #:arguments-guard (and guard (comparator-function-guard guard))
      #:application-marks marks
      #:chaperone? chaperone?))
   (define impersonated-without-props
-    (make-comparator impersonated-function #:name (object-name comparator)))
+    (constructor:comparator
+     #:function impersonated-function
+     #:reverse-function impersonated-reverse-function
+     #:reverse-name (comparator-reverse-name comparator)
+     #:name (object-name comparator)))
   (object-impersonate impersonated-without-props descriptor:comparator
                       #:properties properties))
+
 
 (module+ test
   (test-case (name-string comparator-impersonate)
@@ -342,6 +383,7 @@
     (check-true (impersonator-of? chaperoned real<=>))
     (check-false (chaperone-of? real<=> chaperoned))
     (check-false (impersonator-of? real<=> chaperoned))))
+
 
 (define/name (comparator/c operand-contract*)
   (define operand-contract
