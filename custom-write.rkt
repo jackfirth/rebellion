a077b0c64d1dc0406bebce841586bb2fd715cd48
dented42 <matias@fortlogic.net>
2024-02-25 16:48:43 -0700

    Added support for more kinds of object names.

--- custom-write.rkt
+++ custom-write.rkt
@@ -6,8 +6,9 @@
  (contract-out
   [custom-write-mode/c flat-contract?]
   [custom-write-function/c chaperone-contract?]
+  [object-name/c flat-contract?]
   [make-named-object-custom-write
-   (->* (symbol?) (#:name-getter (-> any/c (or/c symbol? #false)))
+   (->* (symbol?) (#:name-getter (-> any/c (or/c object-name/c #false)))
         custom-write-function/c)]))
 
 ;@------------------------------------------------------------------------------
@@ -16,6 +17,16 @@
 
 (define custom-write-function/c
   (-> any/c output-port? custom-write-mode/c void?))
+
+(define object-name/c (or/c symbol? string? bytes? number? path?))
+
+(define (object-name->string name)
+  (cond
+    [(symbol? name) (symbol->string name)]
+    [(string? name) name]
+    [(bytes? name) (bytes->string/locale name #\�)]
+    [(number? name) (number->string name)]
+    [(path? name) (path->string name)]))
 
 (define (make-named-object-custom-write type-name
                                         #:name-getter [get-name object-name])
@@ -26,6 +37,6 @@
       (define name (get-name this))
       (when name
         (write-string ":")
-        (write-string (symbol->string name)))
+        (write-string (object-name->string name)))
       (write-string ">"))
     (void)))
